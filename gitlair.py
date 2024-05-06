2011a3852b9bf5cc957107fb345aab71e731e2d5
MarketaH <habranova.marketa@gmail.com>
2024-05-06 19:24:04 +0200

    Move the code for running management commands to separate file

--- gitlair.py
+++ gitlair.py
@@ -1,14 +1,6 @@
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.management import execute_from_command_line
 from django.shortcuts import render
 from django.urls import path
-
-import os
-import django
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-django.setup()
 
 
 def index(request):
@@ -20,6 +12,3 @@ urlpatterns = [
 ]
 
 application = WSGIHandler()
-
-if __name__ == "__main__":
-    execute_from_command_line()
---
+++ manage.py
@@ -0,0 +1,6 @@
+from django.core.management import execute_from_command_line
+import os
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    execute_from_command_line()
