6932a5c720a7f10350983e5cc28c79569a8e6cf9
MarketaH <habranova.marketa@gmail.com>
2024-06-22 16:59:20 +0200

    Tell Django how to manage database

--- settings.py
+++ settings.py
@@ -1,11 +1,23 @@
 from pathlib import Path
 
 ROOT_URLCONF = 'gitlair'
+
 DEBUG = True
+
 SECRET_KEY = 'my-secret-key'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [Path(__file__).parent / 'templates'],
     }
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': Path(__file__).parent / 'db.sqlite3',
+    }
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
